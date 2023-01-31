1c25c74f3136b7ce12189c72cece63deff309e58
YohanFr37 <yohan.boucher.yb@gmail.com>
2023-01-31 23:51:05 +0100

    Ajout test.txt

--- euclid.py
+++ euclid.py
@@ -1,42 +1,18 @@
 # CONSTANTES
 import random
-# f = 2^894 * PI
-f = 0XFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE65381FFFFFFFFFFFFFFFF
-#print(f)
-p = (2 ** 1024) - (2 ** 960) - 1 + (2 ** 64) * (f + 129093)
+fichier = open("test.txt", "w")
+# HexaNumber = 2^894 * PI
+HexaNumber = 0XFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE65381FFFFFFFFFFFFFFFF
+p = (2 ** 1024) - (2 ** 960) - 1 + (2 ** 64) * (HexaNumber + 129093)
 g = 2
 
-
-a = random.getrandbits(1024)
-b = random.getrandbits(75)
-
-def pgcd(a, b):
-    print(a, ' ', b)
-    if (a % b != 0):
-        newa = b
-        newb = a % b
-        pgcd(newa, newb)
-    else:
-        print("pgcd ", b)
-        premier(b)
-        return b
-
-
-def premier(b):
-    if (b == 1):
-        print("true ", b)
-        return True
-    else:
-        print("false ", b)
-        return False
-
-def bezout(aa, bb,i):
+def bezout(a, p,i):
     U0 = 1
     U1 = 0
     V0 = 0
     V1 = 1
-    R0 = aa
-    R1 = bb
+    R0 = a
+    R1 = p
     while (R1 > 0):
         Q = R0//R1
         R = R0 % R1
@@ -49,9 +25,18 @@ def bezout(aa, bb,i):
         U1 = U
         V1 = V
         #print("Q ",Q, " R ", R, " U ", U, " V ", V, " U0 ", U0, " U1 ", U1, " V0 ", V0, " V1 ", V1)
-    print("i",i," R0 ",R0 ," aa ", aa)
+    print("i",i," R0 ",R0 ," aa ", a)
+    if (i<=5):
+        fichier.write("i : "+ str(i) +"\n")
+        fichier.write("a : "+ str(a) +"\n")
+        fichier.write("u : "+ str(U0) +"\n")
+        fichier.write("p : "+ str(R1) +"\n")
+        fichier.write("v : "+ str(V0) +"\n")
+        fichier.write("Reste : "+ str(R0)+"\n")
+        fichier.write("\n")
 
-
-for i in range(1000):
-    c = random.getrandbits(1024)
-    bezout(c,p,i+1)
+def main ():    
+    for i in range(100):
+        c = random.getrandbits(1024)
+        bezout(c,p,i+1)
+main()
